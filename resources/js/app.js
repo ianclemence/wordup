bc41c652c5fc2b55e35c8108ef1ad20456100e68
Ian Clemence <ianclemence17@gmail.com>
2022-09-08 11:45:43 +0700

    Dynamic scripts with vanilla js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
-import './bootstrap';
+// import './bootstrap';
+let grid = document.querySelector('#game');
+
+// The number of guesses (3)
+let guessesAllowed = 3;
+// The length of the word (3)
+let wordLength = 3;
+
+
+// Generate 3 rows
+let fragment = document.createDocumentFragment();
+
+let generateGrid = () => {
+    Array.from({ length: guessesAllowed }).forEach(() => {
+        let row = document.createElement('div');
+        row.classList.add('row');
+    
+        Array.from({ length: wordLength }).forEach(() => {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+    
+            row.appendChild(tile);
+        })
+    
+        fragment.appendChild('row');
+    });
+    
+    
+    grid.appendChild(fragment);
+}
+
+// Initialize
+generateGrid();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>WordUp</title>
-    
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="game">
-        <div class="row">
+        {{-- <div class="row">
             <div class="tile">x</div>
             <div class="tile">x</div>
             <div class="tile">x</div>
@@ -27,7 +28,8 @@
             <div class="tile">x</div>
             <div class="tile">x</div>
             <div class="tile">x</div>
-        </div>
+        </div> --}}
     </div>
 </body>
+
 </html>
