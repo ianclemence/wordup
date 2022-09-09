4772c6d5cdd0dab4f0b265350e9f94a091e0c8f6
Ian Clemence <ianclemence17@gmail.com>
2022-09-09 14:13:33 +0700

    The source of truth

---
+++ resources/js/Tile.js
@@ -0,0 +1,11 @@
+export default class Tile {
+    letter = "";
+    status = ""; // correct, present, absent
+    fill(key) {
+        this.letter = key.toLowerCase();
+    }
+
+    empty() {
+        this.letter = "";
+    }
+}
