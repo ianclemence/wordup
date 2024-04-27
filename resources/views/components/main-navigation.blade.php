3988720bf43ff6633556ed48501f065d948f7521
Ian Clemence <batricupid@gmail.com>
2024-04-27 21:25:17 +0700

    Update instructions

--- resources/views/components/main-navigation.blade.php
+++ resources/views/components/main-navigation.blade.php
@@ -17,9 +17,9 @@
         x-show="instructions" @click.away="instructions = false" x-cloak>
 
         {{-- modal inner --}}
-        <div class="relative max-w-2xl rounded-lg bg-gray-100 px-4 py-16 sm:px-6 lg:px-8 shadow-xl dark:bg-gray-900"
-            x-transition:enter="motion-safe:ease-out duration-300"
-            x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100">
+        <div class="relative max-w-2xl rounded-lg bg-gray-100 px-4 py-16 sm:px-6 lg:px-8 shadow-xl dark:bg-gray-900 text-gray-900 dark:text-white"
+            x-transition:enter="motion-safe:ease-out duration-300" x-transition:enter-start="opacity-0 scale-90"
+            x-transition:enter-end="opacity-100 scale-100">
 
             {{-- close modal button --}}
             <div class="absolute top-4 end-4 ">
@@ -32,17 +32,54 @@
             </div>
 
             {{-- content --}}
-            <h1 class="text-center text-3xl sm:text-4xl text-gray-900 dark:text-white">
+            <h1 class="text-center text-3xl sm:text-4xl">
                 How to Play WordUp
             </h1>
 
-            <p class="mx-auto mt-4 mb-8 max-w-md text-center text-lg text-gray-500 dark:text-gray-300">
-                Guess today's mystery word within <strong  class="text-gray-900 dark:text-white"> four </strong> guesses.
+            <p class="mx-auto my-4 max-w-md text-center text-lg">
+                Guess today's mystery word within <strong class="font-bold"> four </strong> guesses.
             </p>
 
-            <ul class="mx-auto mt-4 mb-8 max-w-md text-start text-base text-gray-500 dark:text-gray-300 space-y-5">
-                <li><strong class="text-[#47d747] text-2xl"> Green </strong>: These tiles represent correctly guessed letters in their correct positions within the word.</li>
-                <li><strong class="text-[#f7f749] text-2xl"> Yellow </strong>: These tiles might indicate that you've guessed a correct letter, but it's not in the correct position within the word.</li>
+            <ul class="mx-auto max-w-md text-start text-sm space-y-1 list-disc ml-5">
+                <li>Each guess must be a valid 4-letter word.</li>
+                <li>The color of the tiles will change to show how close your guess was to the word.</li>
+            </ul>
+
+            <p class="mx-auto mt-4 mb-2 max-w-md font-bold text-start text-base">
+                Examples:
+            </p>
+
+            <ul class="mx-auto my-6 max-w-md text-start text-sm space-y-3">
+                <li class="space-x-3">
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        C
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium text-[#47d747]">
+                        A
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        R
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        E
+                    </button>
+                </li>
+                <li><strong class="text-[#47d747] text-2xl"> A </strong> is in the word and in the correct position.</li>
+                <li class="space-x-3">
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        P
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        L
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium">
+                        A
+                    </button>
+                    <button type="button" class="inline-block rounded border p-3 text-1xl font-medium text-[#f7f749]">
+                        Y
+                    </button>
+                </li>
+                <li><strong class="text-[#f7f749] text-2xl"> Y </strong> is in the word but in the wrong position.</li>
             </ul>
 
             <div class="flex flex-col items-center">
