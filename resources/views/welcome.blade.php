47ba4d87303566787cc0811e01ad256f7f5c1fb1
Ian Clemence <batricupid@gmail.com>
2024-04-27 17:55:09 +0700

    Fix styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,58 +10,55 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Styles -->
+    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{-- AlpineJS --}}
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="font-sans antialiased text-gray-900 bg-[#0e1626] grid">
-    <main class="h-full flex flex-col items-center mx-auto max-w-screen-xl" x-data="game" @keyup.window="onKeyPress($event.key)">
-        <h1 class="mt-6 text-center text-4xl font-bold tracking-tight text-white sm:text-5xl">
-            WordUp
-        </h1>
-
-        {{-- Instructions --}}
-        <div class="w-full sm:max-w-md px-6 py-4 text-sm text-white">
-            <ul>
-                <li class="mb-2">
-                    🟢 : a perfect match.
-                </li>
-                <li class="mb-2">
-                    🟡 : the letter is present in the word but in the wrong tile.
-                </li>
-            </ul>
+    <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 ">
+        <div>
+            <a href="/" wire:navigate>
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
         </div>
 
-        <output x-text="message"></output>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 sm:rounded-lg">
+            <div x-data="game" @keyup.window="onKeyPress($event.key)">
 
-        {{-- Game components --}}
-        <div id="game">
-            <template x-for="(row, index) in board">
-                <div class="row"
-                    :class="{ 'current': currentRowIndex === index, 'invalid': currentRowIndex === index && errors }">
-                    <template x-for="tile in row">
-                        <div class="tile" :class="tile.status" x-text="tile.letter"></div>
+                {{-- Game components --}}
+                <div id="game">
+                    <template x-for="(row, index) in board">
+                        <div class="row"
+                            :class="{ 'current': currentRowIndex === index, 'invalid': currentRowIndex === index && errors }">
+                            <template x-for="tile in row">
+                                <div class="tile" :class="tile.status" x-text="tile.letter"></div>
+                            </template>
+                        </div>
                     </template>
                 </div>
-            </template>
-        </div>
 
-        {{-- Keyboard components --}}
-        <div id="keyboard" @click.stop="$event.target.matches('button') && onKeyPress($event.target.textContent)">
-            <template x-for="row in letters">
-                <div class="row">
-                    <template x-for="key in row">
-                        <button type="button" class="key" :class="matchingTileForKey(key)?.status" x-text="key">
-                        </button>
+                {{-- Keyboard components --}}
+                <div class="mt-8 space-y-3" @click.stop="$event.target.matches('button') && onKeyPress($event.target.textContent)">
+                    <template x-for="row in letters">
+                        <div class="flex justify-center">
+                            <template x-for="key in row">
+                                <button type="button" class="key bg-[#c5c5c5] border-0 rounded h-12 px-4 mx-0.5" :class="matchingTileForKey(key)?.status"
+                                    x-text="key">
+                                </button>
+                            </template>
+                        </div>
                     </template>
                 </div>
-            </template>
+
+                {{-- Output --}}
+                <div class="text-white justify-center flex font-medium text-xl mt-4" x-text="message"></div>
+            </div>
         </div>
-    </main>
+    </div>
 </body>
 
 </html>
